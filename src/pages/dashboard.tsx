f3be860bb3df4ca5ef485970fd69aa096d2e0fc3
Pelaez <paulopelaez@hotmail.com>
2021-12-18 20:38:15 -0300

    graficos

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,80 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import ApexOptions from "react-apexcharts";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  colors: [
+    theme.colors.purple[400],
+    theme.colors.purple[500],
+    theme.colors.purple[700],
+    theme.colors.purple[800],
+  ],
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.8,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const candidateSeries = [
+  {
+    name: "series1",
+    data: [31, 120, 10, 28, 61, 18, 109],
+  },
+];
+
+const previewSeries = [
+  {
+    name: "series1",
+    data: [110, 74, 140, 12, 150, 25, 130],
+  },
+];
 
 export default function Dashboard() {
   return (
@@ -10,15 +84,27 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
-              Incritos da Semana
+              Candidatos inscritos
             </Text>
+            <Chart
+              options={options}
+              series={candidateSeries}
+              type="area"
+              height={160}
+            />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
-              Taxa de abertura
+              Taxa de visualização
             </Text>
+            <Chart
+              options={options}
+              series={previewSeries}
+              type="area"
+              height={160}
+            />
           </Box>
         </SimpleGrid>
       </Flex>
